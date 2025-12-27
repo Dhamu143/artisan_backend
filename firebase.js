35142bda2b518d2fdabf15dc99584d82ad32dac4
Dhamu143 <ankurp0608@gmail.com>
2025-12-27 10:36:24 +0530

    first commit

--- firebase.js
+++ firebase.js
@@ -1,9 +1,11 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 module.exports = admin;
